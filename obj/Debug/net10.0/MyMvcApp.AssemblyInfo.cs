c4d7bcb1a37e451b59d4121a50c9a0a3a87d33ca
ronald arias <you@example.com>
2026-05-04 17:03:22 +0800

    merge

--- obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d34174c2d92226fbd0871f35d688260c75437f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e786ad1e4fa6a8f3f0beb845e25c0d79608629a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
